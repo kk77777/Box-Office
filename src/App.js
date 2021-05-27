45ca82c25f632807a7d99be5fe2c655caa6fdf56
kk77777 <kaushikgattani@gmail.com>
2021-05-27 19:02:29 +0530

    installed react-router and react-router-dom packages, setting up routes in the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello World !!</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        Home Page
+      </Route>
+      <Route exact path="/starred">
+        Starred Page
+      </Route>
+      <Route>404! Not Found!</Route>
+    </Switch>
+  );
 }
 
 export default App;
