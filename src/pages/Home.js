457ebd8d72d869b2a51f1dc35e6ea1d124bc5179
kk77777 <kaushikgattani@gmail.com>
2021-05-28 17:56:46 +0530

    displaying api results on page and refactoring fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    fetch(`http://api.tvmaze.com/singlesearch/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => console.log(result));
+    apiGet(`/search/shows?q=${input}`).then(result => setResults(result));
   };
 
   const onInputChange = e => {
@@ -18,6 +18,22 @@ const Home = () => {
     if (e.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results found!</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -31,6 +47,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
